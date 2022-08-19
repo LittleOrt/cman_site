cff6b6a8d57e7c1b062302853dbd777ed9159a8c
LittleOrt <109601889+LittleOrt@users.noreply.github.com>
2022-08-19 08:24:00 +0100

    Index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app1 = express()
+const path = require('path')
+const fs = require('fs')
+const port1 = 2000
+
+
+app1.use(express.urlencoded({ extended: true }))
+app1.use(express.static(path.join(__dirname, 'public')))
+//+8926lahair,LittleOrt D9m1l0l9
+app1.post('/adminLog.html', (req, res) => {
+
+    var check = ["AdeSam", "ForAllNations"]
+    if (req.body.uname == check[0] && req.body.pword == check[1]) {
+        let Evnt = req.body.Evnt
+        fs.writeFile('public/post.txt', Evnt, {encoding:'utf8', flag: 'w'}, (err) => {
+            if (err) 
+            console.log(err)
+          }
+          )
+          res.send('Welcome  ' + req.body.uname + ', Post Successfully Received!')
+    }
+
+    else {
+        res.send('Login Details Incorrect')
+    }
+})
+app1.listen(port1, () => {
+    console.log(`Server listening on ${port1}`)
+})
+
+
