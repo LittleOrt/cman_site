69e3e0256b94d4be651e900ff1a573f77ff72cab
LittleOrt <109601889+LittleOrt@users.noreply.github.com>
2022-08-29 08:38:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app1 = express()
 const path = require('path')
 const fs = require('fs')
-
+const port = process.env.PORT||2000
 
 
 app1.use(express.urlencoded({ extended: true }))
@@ -24,7 +24,7 @@ app1.post('/adminLog.html', (req, res) => {
         res.send('Login Details Incorrect')
     }
 })
-app1.listen(process.env.PORT||2000)
+app1.listen(port)
 })
 
 
